03106b0b591e44145e5800cd70d75b350bf18c28
cj <cgorrell@furbishco.com>
2014-06-09 14:50:29 -0400

    flask dyn links added

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,10 +4,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
 
 @app.route("/")
 @app.route("/hello")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!?!?!"
 if __name__ == "__main__":
     app.run()
